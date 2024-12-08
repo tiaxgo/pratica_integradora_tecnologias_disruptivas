ebdc2db3254d491375103b9431660c95999c0b3e
tiaxgo <161162502+tiaxgo@users.noreply.github.com>
2024-12-07 21:38:20 -0300

    Up

--- modulo14/aplicacao/backend/src/main/java/com/projeto/integrado/repository/UserRepository.java
+++ modulo14/aplicacao/backend/src/main/java/com/projeto/integrado/repository/UserRepository.java
@@ -7,5 +7,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.projeto.integrado.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-	Optional<User> findByUserEmail(String email);
+    Optional<User> findByUserEmail(String email);
+    
+    List<User> findByNome(String nome);
+    
+    List<User> findByNomeContaining(String substring);
+    
+    List<User> findByAtivoTrue();
+    
+    Optional<User> findByUserEmailAndAtivo(String email, boolean ativo);
 }
